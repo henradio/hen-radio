9d301cf86deb008bbc4be050d6845ffc13e587c1
Lauzaki <L@L.local>
2021-08-20 20:26:27 +0100

    draft FAQ

---
+++ pages/faq/index.js
@@ -0,0 +1,19 @@
+const FaqPage = () => {
+
+
+    return (
+        <>
+           
+            <h1>Faq</h1>
+            <div>
+                <p><strong>Q.</strong> How does the minting work on hen radio?<br/>
+                <strong>A.</strong> Mint your audio file (preferably ogg or mp3) as usual on https://www.hicetnunc.xyz/mint </p>
+                
+                <p><strong>Q.</strong> Does everything works as well in visual Hen?<br/>
+                <strong>A.</strong> Yes, everything on Hen.radio will also be available on Hen</p>
+            </div>
+        </>
+    );
+};
+
+export default FaqPage;
