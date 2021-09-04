632ecb16c30903501ec9aca24c9f5caffa078a78
Sarcoma <sarcoma@live.co.uk>
2021-09-05 00:06:25 +0100

    Clean up FAQ

---
+++ pages/faq.js
@@ -0,0 +1,41 @@
+const FaqPage = () =>
+    <>
+        <h1>FAQ</h1>
+        <div>
+            <p><strong>Q. How does the minting work on hen radio?</strong></p>
+            <p> A. Mint your audio file (preferably ogg or mp3) as usual on{' '}
+                <a href="https://www.hicetnunc.xyz/mint">https://www.hicetnunc.xyz/mint</a>
+            </p>
+            <p><strong>Q. Is everything here also work on Hic et Nunc?</strong></p>
+            <p>A. Yes, everything on hen.radio will also be available on the main Hic et Nunc
+               site</p>
+            <p><strong>Q. How do I create a playlist?</strong></p>
+            <p>A1. To create a local playlist (only visible to you):</p>
+            <ol>
+                <li>Go to &quot;Playlists&quot; in the top right menu</li>
+                <li>Enter a title in the textbox &quot;Playlist Title&quot;</li>
+                <li>Click &quot;Create Playlist&quot;</li>
+                <li>Add tracks to the playlist by clicking the little &quot;+&quot; button next
+                    to each track.
+                </li>
+                <li>Clicking the link icon will copy the link to the playlist. You can share
+                    this link via Twitter or email. For example:
+                    {' '}<a href="https://hen.radio/playlists/178891,11305,10159,8228">https://hen.radio/playlists/178891,11305,10159,8228</a>
+                </li>
+            </ol>
+            <p>A2. To create a playlist with a cover image and visible to everybody you can message our twitter account
+               @hen_radio with:</p>
+            <ol>
+                <li>The link to the playlist (see above, eg:
+                    {' '}<a href="https://hen.radio/playlists/178891,11305,10159,8228">https://hen.radio/playlists/178891,11305,10159,8228</a>)
+                </li>
+                <li>Title of the playlist</li>
+                <li>Description</li>
+                <li>The playlist creator alias, name or twitter handle</li>
+                <li>Cover image in jpeg format</li>
+            </ol>
+            <p>Playlists are a work in progress, please be patient, we will do our best to include all submissions.</p>
+        </div>
+    </>;
+
+export default FaqPage;
