5cb6319fdaad685f8a140379f031684c0dd30187
Lauzaki <84672368+lauzaki@users.noreply.github.com>
2021-12-05 20:49:03 +0000

    update blocklist

--- api/get-blocked-lists.js
+++ api/get-blocked-lists.js
@@ -6,7 +6,9 @@ const localWalletBlockList = [
     'tz1aZjMDWKbS2D4LfJFfG1CqLvvXTQzwQiBQ',
     'tz1T4aXc5yNNyBhMRdUtDwzLmsTdjnmsb6Zn',
     'tz1YHVXBpx4WzsbaYhv5VThj1ojcEhVYw1Md',
-    'tz1WHG9AQfeiDaWd31aespoNrxvhSDHhXEVt'
+    'tz1WHG9AQfeiDaWd31aespoNrxvhSDHhXEVt',
+    'tz1fRL3YzHHwN3Q1nqP1WYwCJTE1GsjbQ38Z',
+    'tz1WD52Q8yh27CvitdP8C6Qwo2PVGfTGqURv'
 ];
 
 const localTrackBlockList = [
@@ -14,7 +16,10 @@ const localTrackBlockList = [
     569105,
     570519,
     570664,
-    572044
+    572044,
+    572499,
+    572464,
+    572320
 ];
 
 export const getBlockedTracks = async() => {
