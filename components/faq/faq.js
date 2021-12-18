46110c529c22c8a6fd1e563e73470bf190027fac
Lauzaki <84672368+lauzaki@users.noreply.github.com>
2021-12-18 17:39:49 +0000

    edit faq and moved contributor list from footer to faq

--- components/faq/faq.js
+++ components/faq/faq.js
@@ -5,33 +5,53 @@ const Faq = () =>
         <h1>FAQ</h1>
         <div className={styles.faqItems}>
             <p><strong>Q. What is Hen.radio?</strong></p>
-            <p>A. Hen.radio is Hic et Nunc, or HEN, NFT Music Player. It
-               has been created during the first Hicathon in 2021.
-               It aims at making it easier to discover, listen to and
-               reward NFT music artists. See our
-               presentation <a href="https://docs.google.com/presentation/d/1t7zirrVZ0wHu0Fx6HJeGZzsDzS0s5n4TEwT9QdC-by4/edit?usp=sharing">
-                    <u>here</u></a>.
+            <p>A. Hen.radio is an NFT Music Player and community. It
+                has been created during the first <a href="https://medium.com/@timongty/hic-et-nunc-community-led-growth-through-the-hicathon-5181d8f310e4"><strong>Hicathon</strong></a> in May 2021.
+                <p>In the initial working Group was&nbsp;
+                    <strong>
+                        <a href='https://twitter.com/lauzaki'>@lauzaki</a>
+                        ,{' '}
+                        <a href='https://twitter.com/andreasrau_eu'>@andreasrau_eu</a>
+                        ,{' '}
+                        <a href='https://twitter.com/uvdsc'>@uvdsc</a>
+                        ,{' '}
+                        <a href='https://twitter.com/__orderandchaos'>@__orderandchaos</a>
+                        ,{' '}
+                        <a href='https://twitter.com/webidente'>@webidente</a>
+                        ,{' '}
+                        <a href='https://twitter.com/BabyCommando_'>@BabyCommando_</a>
+                        ,{' '}
+                        <a href='https://twitter.com/jclayton'>@jclayton</a>
+                        ,{' '}
+                        <a href='https://twitter.com/v5mt_tv'>@v5mt</a></strong><br />
+                    Check our Discord for lastest updates.
+                </p>
+                Hen.radio makes it easier to discover, listen to and
+                reward NFT music artists, in the Hen spirit of inclusivity, diversity, multiculturalism and community.
+                <br />
+                See our presentation <a href="https://docs.google.com/presentation/d/1t7zirrVZ0wHu0Fx6HJeGZzsDzS0s5n4TEwT9QdC-by4/edit?usp=sharing">
+                    <u><strong>here</strong></u></a>.
             </p>
             <p><strong>Q. How does the minting work on hen radio?</strong></p>
             <p>A. Sync your wallet using
-               the <strong>sync</strong> button on the top right
-               corner. A <strong>Mint</strong> button will then appear.
-               Find more details on how to mint <a href="https://github.com/henradio/hen-radio/wiki/How-to#how-to-mint">
-                    <u>here</u></a>.
+                the <strong>sync</strong> button on the top right
+                corner. A <strong>Mint</strong> button will then appear.
+                Find more details on how to mint <a href="https://github.com/henradio/hen-radio/wiki/How-to#how-to-mint">
+                    <u><strong>here</strong></u></a>.
             </p>
             <p><strong>Q. Is everything here also work on Hic et Nunc?</strong>
             </p>
             <p>A. Yes, everything on hen.radio will also be available on
-               the main Hic et Nunc site</p>
+                the main Hic et Nunc site</p>
             <p><strong>Q. Is there an app?</strong></p>
             <p>A. Not at the moment. It is optimised for viewing in the
-               mobile browser, but no app yet.</p>
+                mobile browser, but no app yet.</p>
             <p><strong>Q. Why don&apos;t you support video or html
-                       music?</strong></p>
+                music?</strong></p>
             <p>A. Currently technical restrictions are making it
-               difficult to differenciate music HTML from other HTML
-               objkt. For video, the is different than the handling of
-               audio, so it is also difficult to implement.</p>
+                difficult to differenciate music HTML from other HTML
+                objkt. For video, the is different than the handling of
+                audio, so it is also difficult to implement.</p>
             <p><strong>Q. How do I create a playlist?</strong></p>
             <p>A1. To create a local playlist (only visible to you):</p>
             <ol>
@@ -52,8 +72,8 @@ const Faq = () =>
                 </li>
             </ol>
             <p>A2. To create a playlist with a cover image and visible
-               to everybody you can message our twitter account
-               @hen_radio with:</p>
+                to everybody you can message our twitter account
+                @hen_radio with:</p>
             <ol>
                 <li>The link to the playlist (see above, eg:
                     {' '}
@@ -67,17 +87,12 @@ const Faq = () =>
                 <li>Cover image in jpeg format</li>
             </ol>
             <p>Playlists are a work in progress, please be patient, we
-               will do our best to include all submissions.</p>
+                will do our best to include all submissions.</p>
             <p><strong>Q. How can I contribute? How do we contact
-                       Hen.radio?</strong></p>
-            <p>A.</p>
-            <ul>
-                <li>You can find us on Discord: <a href="https://discord.gg/gj7U6r96aF"><strong>hen-radio</strong></a></li>
-                <li>Twitter: <a href="https://twitter.com/hen_radio"><strong>@hen_radio</strong></a>
-                </li>
-                <li>Github: <a href="https://github.com/henradio/hen-radio"><strong>hen-radio</strong></a>
-                </li>
-            </ul>
+                Hen.radio?</strong></p>
+            <p>A.
+                The best place to engage with us is through the Discord, see contacts below.
+            </p>
         </div>
     </>;
 
